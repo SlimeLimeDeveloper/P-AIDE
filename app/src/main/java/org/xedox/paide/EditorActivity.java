9df08abb5e92909bb108f605c66392164ef377c8
SlimeLimeDeveloper <slimelime00123@gmail.com>
2024-07-30 17:56:13 +0700

    commit

--- app/src/main/java/org/xedox/paide/EditorActivity.java
+++ app/src/main/java/org/xedox/paide/EditorActivity.java
@@ -1,6 +1,9 @@
 package org.xedox.paide;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import io.github.rosemoe.sora.widget.CodeEditor;
@@ -17,7 +20,21 @@ public class EditorActivity extends AppCompatActivity {
 
         editor = findViewById(R.id.editor);
         toolbar = findViewById(R.id.toolbar);
-        
+
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.editor_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.run_code) {
+            Log.w("Run item", " click");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
